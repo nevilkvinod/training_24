7fbcbb5260dbdb50a67b8851eeae0258a31e0a79
PARVEZ KHAN <parvezkhan@anorasolutions.com>
2024-08-14 12:52:58 +0530

    Multiplication of matrix

---
+++ max_min/GccApplication1/config.c
@@ -0,0 +1,7 @@
+/*
+ * CFile1.c
+ *
+ * Created: 01-08-2024 11:47:13
+ *  Author: PARVEZ KHAN
+ */ 
+int max_min = 1;
---
+++ max_min/GccApplication1/config.h
@@ -0,0 +1,14 @@
+/*
+ * IncFile1.h
+ *
+ * Created: 01-08-2024 11:45:13
+ *  Author: PARVEZ KHAN
+ */ 
+
+// config.h
+#ifndef CONFIG_H
+#define CONFIG_H
+
+extern int max_min;
+
+#endif // CONFIG_H

3ed103c1769a854a8a08c787be1adfd191803b35
Anora_TR_07\AMALKRISHNA S <amalkrishnas@anorasolutions.com>
2024-08-16 12:16:52 +0530

    adding c file for the duplicate element in a array

---
+++ Amalkrishna/duplicate.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+void find_duplicate(int* ,int);
+int duplicate;
+
+int main(){
+    int array_size;
+    
+    printf("\nEnter the array size : ");
+    scanf("%d",&array_size);
+    int array[array_size];
+
+    for (int i=0;i<array_size;i++){
+        printf("\nEnter the element array[%d] : ",i+1);
+        scanf("%d",&array[i]);
+    }
+
+    find_duplicate(array,array_size);
+    printf("\nThe first duplicate element found is %d .",duplicate);
+    return 0;
+}
+
+
+void find_duplicate(int* ptr ,int array_size){
+    int flag=0;
+    int i=0,j=0;
+    
+    while((i<array_size)&&(flag==0)){
+        j=i+1;
+        while(j<array_size){
+            if (*(ptr+i)==*(ptr+j)){
+                flag=1;
+                duplicate =*(ptr+i);
+                break;
+            }
+            j++;
+        }
+        i++;
+    }
+}

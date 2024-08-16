29bd8b298cab7153c8faf312b72fc5a99e98a05b
PARVEZ KHAN <parvezkhan@anorasolutions.com>
2024-08-16 11:55:47 +0530

    Adding Vowel Count

---
+++ pervez/Vowel_Count/GccLibrary1/library.c
@@ -0,0 +1,16 @@
+/*
+ * GccLibrary1.c
+ *
+ * Created: 31-07-2024 12:50:39
+ * Author : PARVEZ KHAN
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+

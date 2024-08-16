dc8c9db14c0f44d76f1281ea250ba9c196fada37
Parvez9036 <parvezkhan@anorasolutions.com>
2024-08-16 09:45:09 +0530

    Delete adc directory

--- adc/GccApplication2/Debug/dac.d
+++
@@ -1,9 +0,0 @@
-dac.d dac.o: .././dac.c .././dac.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h
-
-.././dac.h:
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:

16c0497666c1841bd1cab76ac34b54e90e4b3669
Anora_TR_16\Siyan Ananth A <siyananantha@anorasolutions.com>
2024-08-16 11:18:04 +0530

    Added all my atmel project

---
+++ siyan/findgcd/findgcd/main.c
@@ -0,0 +1,17 @@
+/*
+ * findgcd.c
+ *
+ * Created: 03-08-2024 11:46:09
+ * Author : Siyan Ananth A
+ */ 
+
+#include <avr/io.h>
+
+int gcd(int ,int);
+
+int main(void)
+{
+    int a=20,b=100;
+	int c=gcd(a,b);
+}
+

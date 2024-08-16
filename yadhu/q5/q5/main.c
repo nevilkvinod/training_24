981263a9309626fc564814a3a48d36d6b76ac849
Anora_TR_01\YADHU KRISHNA T <yadhukrishnat@anorasolutions.com>
2024-08-16 12:09:36 +0530

    adding q5

---
+++ yadhu/q5/q5/main.c
@@ -0,0 +1,17 @@
+/*
+ * q5.c
+ *
+ * Created: 31-07-2024 10:20:24
+ * Author : YADHU KRISHNA T
+ */ 
+
+#include <avr/io.h>
+
+int func(int,int,int);
+int main(void)
+{
+	int val;
+    /* Replace with your application code */
+	val=func(181,2,5);
+}
+

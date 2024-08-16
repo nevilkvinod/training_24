436ecfa82de08c1b73aff33db4ad01988f2a188c
Anora_TR_01\YADHU KRISHNA T <yadhukrishnat@anorasolutions.com>
2024-08-16 12:34:15 +0530

    adding new file

---
+++ yadhu/q4/q4/main.c
@@ -0,0 +1,18 @@
+/*
+ * q4.c
+ *
+ * Created: 31-07-2024 09:53:57
+ * Author : YADHU KRISHNA T
+ */ 
+
+#include <avr/io.h>
+
+void vowel(char*);
+int main(void)
+{
+	char* c="MISSISSIPPI";
+	vowel(c);
+	extern int vowel_count;
+	
+}
+

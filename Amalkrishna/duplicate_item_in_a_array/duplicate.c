891606c99e757ad936b2c2d20f6f727e5d1a7298
Anora_TR_07\AMALKRISHNA S <amalkrishnas@anorasolutions.com>
2024-08-19 12:35:45 +0530

    resolving conflict with amal

--- Amalkrishna/duplicate_item_in_a_array/duplicate.c
+++ Amalkrishna/duplicate_item_in_a_array/duplicate.c
@@ -32,6 +32,7 @@ void find_duplicate(int* ptr ,int array_size){
             if (*(ptr+i)==*(ptr+j)){
               flag=1;
                 duplicate =*(ptr+i);
+                break;
             }
             j++;
         }

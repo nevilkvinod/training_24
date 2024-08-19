f26aac2a7b2bcf45e011474bdb26c2a1960431bd
Anora_TR_07\AMALKRISHNA S <amalkrishnas@anorasolutions.com>
2024-08-19 12:29:17 +0530

    resolving conflict 2

--- Amalkrishna/duplicate_item_in_a_array/duplicate.c
+++ Amalkrishna/duplicate_item_in_a_array/duplicate.c
@@ -30,8 +30,12 @@ void find_duplicate(int* ptr ,int array_size){
         j=i+1;
         while(j<array_size){
             if (*(ptr+i)==*(ptr+j)){
+<<<<<<< HEAD
             //    ******flag=1;
                //flag=1;
+=======
+              flag=1;
+>>>>>>> 9de60ca (resolving conflict 2)
                 duplicate =*(ptr+i);
                 break;
             }

5ed9c7386ed1ac646f1bc42cf7bd3b24ce19acef
Anora_TR_01\YADHU KRISHNA T <yadhukrishnat@anorasolutions.com>
2024-08-20 12:43:57 +0530

    just checking

---
+++ sample/atmel_group_project/groupwork/groupwork/main.c
@@ -0,0 +1,37 @@
+/*
+ * groupwork.c
+ *
+ * Created: 20-08-2024 12:26:10
+ * Author : Siyan Ananth A
+ */ 
+
+#include <avr/io.h>
+void yethu(){
+	
+}
+void kavi(){
+	
+}
+void parvez(){
+	
+}
+void amul(){
+	
+}
+void siyan(){
+	
+}
+void adhi(){
+	
+}
+int main(void)
+{
+    yethu();
+	kavi();
+	parvez();
+	amul();
+	siyan();
+	adhi();
+	
+}
+

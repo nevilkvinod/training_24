8e2ab968c57beadecf8d5a4e958f9ba14336ca1b
Anora_TR_16\Siyan Ananth A <siyananantha@anorasolutions.com>
2024-08-21 11:48:41 +0530

    created a file inside folder spk and created a atmel project inside that folder

---
+++ spk/createconflict/createconflict/main.c
@@ -0,0 +1,19 @@
+/*
+ * createconflict.c
+ *
+ * Created: 21-08-2024 11:45:18
+ * Author : Siyan Ananth A
+ */ 
+
+#include <avr/io.h>
+
+void addition(){
+	int a=10,b=20,c;
+	c=a+b;
+}
+
+void main()
+{
+    addition();
+}
+
